4fc43a64aa0587cd399f82f44512bb422054df39
Roshan Bhagat <104725121+roshan0112@users.noreply.github.com>
2022-06-21 16:57:29 +0545

    Add files via upload

---
+++ manage_report.php
@@ -0,0 +1,57 @@
+<?php session_start() ?>
+<div class="container-fluid">
+	<form action="" id="complaint-frm" method = "post" enctype = "multipart/form-data">
+		<input type="hidden" name="id" value="">
+		<div class="form-group">
+			<label for="" class="control-label">Report Message</label>
+			<textarea cols="30" rows="3" name="message" required="" class="form-control"></textarea>
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Incident Address</label>
+			<textarea cols="30" rows="3" name="address" required="" class="form-control"></textarea>
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Evidence</label>
+			<input type = "file" name = "evidence" required = "" class="form-control"> 
+		</div>
+		<button class="button btn btn-primary btn-sm">Create</button>
+		<button class="button btn btn-secondary btn-sm" type="button" data-dismiss="modal">Cancel</button>
+
+	</form>
+</div>
+
+<style>
+	#uni_modal .modal-footer{
+		display:none;
+	}
+	
+</style>
+<script>
+	$('#complaint-frm').submit(function(e){
+		e.preventDefault()
+		start_load()
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'admin/ajax.php?action=complaint',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#complaint-frm button[type="submit"]').removeAttr('disabled').html('Create');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.reload();
+					alert_toast("Data successfully saved.",'success')
+						setTimeout(function(){
+							location.reload()
+						},1000)
+				}else{
+					end_load()
+				}
+			}
+		})
+	})
+</script>
